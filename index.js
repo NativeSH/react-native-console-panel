95959df352487be87ee036ab72d205bef25bb93b
sospartan <sospartan@gmail.com>
2015-11-20 14:31:03 +0800

    添加协议

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 /**
+ * index.js
+ * Copyright (c) 2015 Alibaba Group Holding Limited
+ * Authors:
+ *  sospartan <sospartan@alibaba-inc.com> (https://github.com/sospartan)
+ *
+ * This file is part of react native console panel.
+ *
+ * react native console panel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * react native console panel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with react native console panel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * Created by sospartan on 11/18/15.
  */
 
